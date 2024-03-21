6e5f1de7c86b223b1c2ec16c22b80c9b90c07cb1
JarlissonTomaz <tomazj995@gmail.com>
2024-03-21 16:36:37 -0300

    text fild

--- lib/main.dart
+++ lib/main.dart
@@ -37,74 +37,96 @@ class MyHomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Expenses',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.purple[300],
+      appBar: AppBar(
+        title: const Text(
+          'Expenses',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(
-              width: double.infinity,
-              child: Card(
-                elevation: 5,
-                color: Colors.purple,
-                child: Text(
-                  'Gráfico',
-                  style: TextStyle(color: Colors.white),
-                ),
+        backgroundColor: Colors.purple[300],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            width: double.infinity,
+            child: Card(
+              elevation: 5,
+              color: Colors.purple,
+              child: Text(
+                'Gráfico',
+                style: TextStyle(color: Colors.white),
               ),
             ),
-            Column(
-                children: _transactions.map((tr) {
+          ),
+          Column(
+              children: _transactions.map(
+            (tr) {
               return Card(
-                  child: Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
                       ),
-                    ),
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 15, vertical: 10),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      'R\$ ${tr.value.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                        color: Colors.purple,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tr.title,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 10),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
                         style: const TextStyle(
-                          fontSize: 16,
+                          color: Colors.purple,
+                          fontSize: 20,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: const TextStyle(
-                          color: Colors.grey,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              ));
-            }).toList()),
-          ],
-        ));
+                        Text(
+                          DateFormat('d MMM y').format(tr.date),
+                          style: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          ).toList()),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                const TextField(
+                  decoration: InputDecoration(hintText: 'Título'),
+                ),
+                const TextField(
+                  decoration: InputDecoration(hintText: 'Valor (R\$)'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    return null;
+                  },
+                  child: const Text('Nova transação'),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
